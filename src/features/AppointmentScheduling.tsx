646749df60ef159788c71d348f04bb3a60563709
rasanjana nimsara <rnimsara54@gmail.com>
2026-04-17 10:37:54 +0530

    Create appointment scheduling system

---
+++ src/features/AppointmentScheduling.tsx
@@ -0,0 +1,13 @@
+// AppointmentScheduling.tsx
+import React from 'react';
+
+const AppointmentScheduling: React.FC = () => {
+    return (
+        <div>
+            <h1>Appointment Scheduling System</h1>
+            {/* Add scheduling functionality here */}
+        </div>
+    );
+};
+
+export default AppointmentScheduling;
