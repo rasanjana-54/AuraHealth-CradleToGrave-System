7c17fbf27bb9bc7cd5c55d4573d84b34943dbafc
rasanjana nimsara <rnimsara54@gmail.com>
2026-04-17 10:38:06 +0530

    Implement responsive dashboard layout

---
+++ src/layouts/Dashboard.tsx
@@ -0,0 +1,15 @@
+// Dashboard.tsx
+
+import React from 'react';
+import './Dashboard.css';
+
+const Dashboard: React.FC = () => {
+    return (
+        <div className="dashboard">
+            <h1>Dashboard</h1>
+            {/* Additional components and layout here */}
+        </div>
+    );
+};
+
+export default Dashboard;
