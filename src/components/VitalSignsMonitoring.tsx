759b21c47e89ce3ff70a1e55cd565bf7b81eb2f5
rasanjana nimsara <rnimsara54@gmail.com>
2026-04-17 10:38:20 +0530

    Add vital signs monitoring feature

---
+++ src/components/VitalSignsMonitoring.tsx
@@ -0,0 +1,13 @@
+// VitalSignsMonitoring.tsx
+import React from 'react';
+
+const VitalSignsMonitoring = () => {
+    return (
+        <div>
+            <h1>Vital Signs Monitoring</h1>
+            {/* Implementation goes here */}
+        </div>
+    );
+};
+
+export default VitalSignsMonitoring;
